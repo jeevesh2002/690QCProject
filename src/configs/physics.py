ed1343b4cf846c7f1602ce38d89e00e8927f1377
Jeevesh <jarigala@umass.edu>
2025-04-29 15:35:37 -0400

    add physical constants

---
+++ src/configs/physics.py
@@ -0,0 +1,7 @@
+"""Physical constants and default parameters used by the simulator."""
+
+SPEED_OF_LIGHT_FIBER = 2e8          # m/s, refractive index ~1.468
+ATTENUATION_LENGTH_KM = 22          # Beer–Lambert attenuation length
+F0_LINK = 0.85                      # Fidelity of an elementary link given perfect detectors
+DEFAULT_COHERENCE_TIME_S = 1e3      # Memory T_coh
+DETECTION_EFFICIENCY = 0.9
